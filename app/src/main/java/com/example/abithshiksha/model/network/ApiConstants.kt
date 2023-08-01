d89a6f166dc540ef883edf7c2ff09e972a762fa3
subhrajit <subhrajit.deb@ekodus.com>
2023-08-01 17:31:12 +0530

    new commit - 1/8/23

--- app/src/main/java/com/example/abithshiksha/model/network/ApiConstants.kt
+++ app/src/main/java/com/example/abithshiksha/model/network/ApiConstants.kt
@@ -4,8 +4,8 @@ object ApiConstants {
     /*var BASE_URL = "http://abhithsiksha.com/api/"
     var PUBLIC_URL = "http://abhithsiksha.com/"*/
 
-    var BASE_URL = "http://134.209.150.173/api/"
-    var PUBLIC_URL = "http://134.209.150.173/"
+    var BASE_URL = "http://abhithsiksha.com/api/"
+    var PUBLIC_URL = "http://abhithsiksha.com/"
 
     var TEST_URL = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
 }
